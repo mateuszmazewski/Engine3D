3f26dd61170a61cda8a33ab52e765476add6ca12
Mateusz Mazewski <mateusz.mazewski.stud@pw.edu.pl>
2022-04-15 15:11:29 +0200

    Add basic renderer

---
+++ src/main/java/Display.java
@@ -0,0 +1,95 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Display extends Canvas implements Runnable {
+    private static final int FRAMES_PER_SECOND = 60;
+
+    private Thread thread;
+    private final JFrame frame;
+    private final String title = "Engine 3D";
+
+    private final static int WIDTH = 800;
+    private final static int HEIGHT = 600;
+    private static boolean running = false;
+
+    public Display() {
+        frame = new JFrame(title);
+        Dimension dimension = new Dimension(WIDTH, HEIGHT);
+        setPreferredSize(dimension);
+        frame.add(this);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+
+        start();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this, "display");
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        long timer = System.currentTimeMillis();
+        final double frameTime = 1000000000.0 / FRAMES_PER_SECOND;
+        double delta = 0; // when reaches 1 -> update the frame
+        long frames = 0;
+        long now;
+
+        while (running) {
+            now = System.nanoTime();
+            delta += (now - lastTime) / frameTime;
+            lastTime = now;
+
+            while (delta >= 1) {
+                update();
+                delta--;
+            }
+
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                frame.setTitle(title + " | " + frames + " FPS");
+                frames = 0;
+            }
+        }
+
+        stop();
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics graphics = bs.getDrawGraphics();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, WIDTH, HEIGHT);
+
+        graphics.dispose();
+        bs.show();
+    }
+
+    public void update() {
+
+    }
+}
--- src/main/java/Engine3D.java
+++ src/main/java/Engine3D.java
@@ -1,2 +1,7 @@
 public class Engine3D {
+
+    public static void main(String[] args) {
+        Display display = new Display();
+    }
+
 }
